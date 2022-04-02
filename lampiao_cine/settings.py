21d97caef76dad4c5c135af797e9b93244ffb053
Anderson Alencar <alencarbarros@protonmail.com>
2022-04-02 11:45:44 -0300

    deploy done

--- lampiao_cine/settings.py
+++ lampiao_cine/settings.py
@@ -93,9 +93,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
