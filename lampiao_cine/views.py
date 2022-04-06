19b258fe93a3a81082afd49b4ccb19cebca3edc8
Edon Souza <e.brendon30@outlook.com>
2022-04-06 01:53:10 -0300

    update templates

--- lampiao_cine/views.py
+++ lampiao_cine/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 
 # Create your views here.
-
 def home(request):
     return render(request, 'index.html')
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
--- users/models.py
+++ users/models.py
@@ -2,11 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Account(models.Model):
-    email = models.CharField(max_length=250, unique=True)
-    password = models.CharField(max_length=200)
-
-
 class Profile(models.Model):
     name = models.CharField(max_length=50, null=True)
     lastName = models.CharField(max_length=250, null=True)
@@ -20,3 +15,7 @@ class Profile(models.Model):
 class WatchList(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     movies = models.ManyToManyField('cinema.Movie')
+
+class Account(models.Model):
+    email = models.CharField(max_length=250, unique=True)
+    password = models.CharField(max_length=200)
